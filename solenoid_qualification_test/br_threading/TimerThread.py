96fc35b54c388d256186e1d2a0e155b0331220d1
Ethan Subasic <ethansubasic@brick.ucalgary.ca>
2024-09-30 16:02:17 -0600

    initial commit for having the plc test running, warning IP address

---
+++ solenoid_qualification_test/br_threading/TimerThread.py
@@ -0,0 +1,19 @@
+
+
+import threading
+
+class TimerThread(threading.Thread):
+    def __init__(self, target, delay: float, event = None):
+        threading.Thread.__init__(self, daemon=True)
+        self.target = target
+        self.delay = delay
+        if event is None:
+            self.stopped = threading.Event()
+        else:
+            self.stopped = event
+
+    def run(self):
+        while not self.stopped.wait(self.delay):
+            print("my thread")
+            self.target()
+            # call a function
