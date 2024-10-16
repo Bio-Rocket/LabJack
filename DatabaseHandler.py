02548f4b7d35fe39d4fefdbe49ab9d620047f05b
Ethan Subasic <ethansubasic@brick.ucalgary.ca>
2024-10-15 20:56:01 -0600

    adding all plc commands and changing db IP to the DB PI

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -7,6 +7,7 @@ from pocketbase.services.realtime_service import MessageData
 
 from br_threading.WorkQCommands import WorkQCmnd, WorkQCmnd_e
 
+LOADCELL_COMMANDS = ["TARE", "CANCEL", "CALIBRATE", "FINISH"]
 
 # Class Definitions ===============================================================================
 class DatabaseHandler():
@@ -19,28 +20,62 @@ class DatabaseHandler():
         to be read by the front end.
         """
         DatabaseHandler.db_thread_workq = db_thread_workq
-        # DatabaseHandler.client = Client('http://192.168.0.70:8090')
-        DatabaseHandler.client = Client('http://192.168.0.130:8090')
-        # DatabaseHandler.client = Client('http://127.0.0.1:8090')
+        DatabaseHandler.client = Client('http://192.168.0.69:8090') # Database Pi IP
 
-        DatabaseHandler.client.collection('PlcCommands').subscribe(DatabaseHandler._handle_command_callback)
+        DatabaseHandler.client.collection('PlcCommands').subscribe(DatabaseHandler._handle_plc_command_callback)
+        DatabaseHandler.client.collection('LabJack1Commands').subscribe(DatabaseHandler._handle_lj1_command_callback)
+        DatabaseHandler.client.collection('LabJack2Commands').subscribe(DatabaseHandler._handle_lj2_command_callback)
+        DatabaseHandler.client.collection('Heartbeat').subscribe(DatabaseHandler._handle_heartbeat_callback)
 
         print("DB - thread started")
 
     @staticmethod
-    def _handle_command_callback(document: MessageData):
+    def _handle_plc_command_callback(document: MessageData):
         """
-        Whenever a new entry is created in the CommandMessage
+        Whenever a new entry is created in the PlcCommands
         collection, this function is called to handle the
         command and forward it to the serial port.
 
         Args:
             document (MessageData): the change notification from the database.
         """
+        DatabaseHandler.db_thread_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_DB_COMMAND, (document.record.command, document.record.loadcell, document.record.value)))
 
-        print(f"DB - Received {document.record.command} command")
-        message = WorkQCmnd(WorkQCmnd_e.COMMAND_FROM_DB, document.record.command)
-        DatabaseHandler.db_thread_workq.put(message)
+    @staticmethod
+    def _handle_lj1_command_callback(document: MessageData):
+        """
+        Whenever a new entry is created in the LabJack1Commands
+        collection, this function is called to handle the
+        command and forward it to the LabJack1.
+
+        Args:
+            document (MessageData): the change notification from the database.
+        """
+        DatabaseHandler.db_thread_workq.put(WorkQCmnd(WorkQCmnd_e.LJ1_DB_COMMAND, (document.record.command, document.record.value)))
+
+    @staticmethod
+    def _handle_lj2_command_callback(document: MessageData):
+        """
+        Whenever a new entry is created in the LabJack2Commands
+        collection, this function is called to handle the
+        command and forward it to the LabJack2.
+
+        Args:
+            document (MessageData): the change notification from the database.
+        """
+        DatabaseHandler.db_thread_workq.put(WorkQCmnd(WorkQCmnd_e.LJ2_DB_COMMAND, (document.record.command, document.record.value)))
+
+    @staticmethod
+    def _handle_heartbeat_callback(document: MessageData):
+        """
+        Whenever a new entry is created in the Heartbeat
+        collection, this function is called to handle the
+        command and forward it to the serial port.
+
+        Args:
+            document (MessageData): the change notification from the database.
+        """
+        DatabaseHandler.db_thread_workq.put(WorkQCmnd(WorkQCmnd_e.DB_HEART_BEAT, None))
 
     @staticmethod
     def write_plc_data(plc_data: Tuple[bytes]):
@@ -50,9 +85,49 @@ class DatabaseHandler():
             return
 
         entry = {}
-        entry["plc_tc_data"] = list(plc_data[0])
-        entry["plc_pt_data"] = list(plc_data[1])
-        entry["plc_valve_data"] = list(plc_data[2])
+
+        tc_data = list(plc_data[0])
+        entry["TC1"] = tc_data[0]
+        entry["TC2"] = tc_data[1]
+        entry["TC3"] = tc_data[2]
+        entry["TC4"] = tc_data[3]
+        entry["TC5"] = tc_data[4]
+        entry["TC6"] = tc_data[5]
+        entry["TC7"] = tc_data[6]
+        entry["TC8"] = tc_data[7]
+        entry["TC9"] = tc_data[8]
+
+        entry["LC1"] = tc_data[9]
+        entry["LC2"] = tc_data[10]
+
+        pt_data = list(plc_data[1])
+        entry["PT1_voltage"] = pt_data[0]
+        entry["PT2_voltage"] = pt_data[1]
+        entry["PT3_voltage"] = pt_data[2]
+        entry["PT4_voltage"] = pt_data[3]
+        entry["PT5_voltage"] = pt_data[4]
+        entry["PT6_voltage"] = pt_data[5]
+        entry["PT15_voltage"] = pt_data[6]
+        entry["PT16_voltage"] = pt_data[7]
+
+        valve_data = list(plc_data[2])
+        entry["PBV1_state"] = valve_data[0]
+        entry["PBV2_state"] = valve_data[1]
+        entry["PBV3_state"] = valve_data[2]
+        entry["PBV4_state"] = valve_data[3]
+        entry["PBV5_state"] = valve_data[4]
+        entry["PBV6_state"] = valve_data[5]
+        entry["PBV7_state"] = valve_data[6]
+        entry["PBV8_state"] = valve_data[7]
+
+        entry["PMP1_state"] = valve_data[8]
+        entry["PMP2_state"] = valve_data[9]
+        entry["PMP3_state"] = valve_data[10]
+
+        entry["IGN1_state"] = valve_data[11]
+        entry["IGN2_state"] = valve_data[12]
+
+        entry["Heater_state"] = valve_data[13]
 
         try:
             DatabaseHandler.client.collection("PLC").create(entry)
@@ -80,10 +155,64 @@ def process_database_command(command: str, plc_workq: mp.Queue) -> None:
         command (str):
             The command from the database.
     """
-    if command == "OPEN_SOL":
-        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_OPEN_SOL, 1)) # Open PLC solenoid 1
-    elif command == "CLOSE_SOL":
-        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_CLOSE_SOL, 1)) # Close PLC solenoid 1
+    if command == "PBV1_OPEN":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_OPEN_PBV, 1))
+    elif command == "PBV2_OPEN":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_OPEN_PBV, 2))
+    elif command == "PBV3_OPEN":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_OPEN_PBV, 3))
+    elif command == "PBV4_OPEN":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_OPEN_PBV, 4))
+    elif command == "PBV5_OPEN":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_OPEN_PBV, 5))
+    elif command == "PBV6_OPEN":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_OPEN_PBV, 6))
+    elif command == "PBV7_OPEN":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_OPEN_PBV, 7))
+    elif command == "PBV8_OPEN":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_OPEN_PBV, 8))
+    elif command == "PMP1_ON":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_PUMP_ON, 1))
+    elif command == "PMP2_ON":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_PUMP_ON, 2))
+    elif command == "PMP3_ON":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_PUMP_ON, 3))
+    elif command == "IGN1_ON":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_IGN_ON, 2))
+    elif command == "IGN2_ON":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_IGN_ON, 1))
+    elif command == "HEATER_ON":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_HEATER_ON, None))
+
+    elif command == "PBV1_CLOSE":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_CLOSE_PBV, 1))
+    elif command == "PBV2_CLOSE":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_CLOSE_PBV, 2))
+    elif command == "PBV3_CLOSE":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_CLOSE_PBV, 3))
+    elif command == "PBV4_CLOSE":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_CLOSE_PBV, 4))
+    elif command == "PBV5_CLOSE":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_CLOSE_PBV, 5))
+    elif command == "PBV6_CLOSE":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_CLOSE_PBV, 6))
+    elif command == "PBV7_CLOSE":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_CLOSE_PBV, 7))
+    elif command == "PBV8_CLOSE":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_CLOSE_PBV, 8))
+    elif command == "PMP1_OFF":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_PUMP_OFF, 1))
+    elif command == "PMP2_OFF":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_PUMP_OFF, 2))
+    elif command == "PMP3_OFF":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_PUMP_OFF, 3))
+    elif command == "IGN1_OFF":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_IGN_OFF, 2))
+    elif command == "IGN2_OFF":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_IGN_OFF, 1))
+    elif command == "HEATER_OFF":
+        plc_workq.put(WorkQCmnd(WorkQCmnd_e.PLC_HEATER_OFF, None))
+
     return
 
 
@@ -98,8 +227,17 @@ def process_workq_message(message: WorkQCmnd, plc_workq: mp.Queue) -> bool:
     if message.command == WorkQCmnd_e.KILL_PROCESS:
         print("DB - Received kill command")
         return False
-    elif message.command == WorkQCmnd_e.COMMAND_FROM_DB:
-        process_database_command(message.data, plc_workq)
+    elif message.command == WorkQCmnd_e.PLC_DB_COMMAND:
+        if message.data[0] in LOADCELL_COMMANDS:
+            print("LOAD_CELL_COMMAND NOT YET SUPPORTED") # TODO
+        else:
+            process_database_command(message.data, plc_workq)
+    elif message.command == WorkQCmnd_e.LJ1_DB_COMMAND:
+        pass # TODO
+    elif message.command == WorkQCmnd_e.LJ2_DB_COMMAND:
+        pass # TODO
+    elif message.command == WorkQCmnd_e.DB_HEART_BEAT:
+        pass # TODO
     elif message.command == WorkQCmnd_e.PLC_DATA:
         DatabaseHandler.write_plc_data(message.data)
     elif message.command == WorkQCmnd_e.LJ1_DATA:
@@ -107,7 +245,7 @@ def process_workq_message(message: WorkQCmnd, plc_workq: mp.Queue) -> bool:
 
     return True
 
-def database_thread(db_workq: mp.Queue, plc_workq: mp.Queue) -> None:
+def database_thread(db_workq: mp.Queue, plc_workq: mp.Queue) -> None: #TODO: will need workq for LabJack
     """
     The main loop of the database handler. It subscribes to the CommandMessage collection
     """
--- br_threading/WorkQCommands.py
+++ br_threading/WorkQCommands.py
@@ -4,16 +4,29 @@ from typing import Any
 
 class WorkQCmnd_e(Enum):
     KILL_PROCESS = 0 # Kill the process
-    HEART_BEAT = auto() # Heart beat
 
-    COMMAND_FROM_DB = auto() # Command from the database
+    PLC_DB_COMMAND = auto() # Command from the database
+    LJ1_DB_COMMAND = auto() # Command from the LabJack1
+    LJ2_DB_COMMAND = auto() # Command from the LabJack2
+    DB_HEART_BEAT = auto() # Heart beat
 
     LJ1_DATA = auto() # Log LabJack data to csv
 
     PLC_REQUEST_DATA = auto() # Request data from the PLC
     PLC_DATA = auto() # Includes TC, PT, and Valve data in a bytes tuple
-    PLC_OPEN_SOL = auto() # Open the solenoid
-    PLC_CLOSE_SOL = auto() # Close the solenoid
+
+    PLC_OPEN_PBV = auto() # Open the Pneumatic Ball Valve, expects the PBV number
+    PLC_CLOSE_PBV = auto() # Close the Pneumatic Ball Valve, expects a PBV number
+
+    PLC_PUMP_ON = auto() # Turn on the pump, expects the pump number
+    PLC_PUMP_OFF = auto() # Turn off the pump, expects the pump number
+
+    PLC_IGN_ON = auto() # Turn on the igniter, expects the igniter number
+    PLC_IGN_OFF = auto() # Turn off the igniter, expects the igniter number
+
+    PLC_HEATER_ON = auto() # Turn on the heater
+    PLC_HEATER_OFF = auto() # Turn off the heater
+
 
 class WorkQCmnd:
     def __init__(self, command: WorkQCmnd_e, data: Any):
